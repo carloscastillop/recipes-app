23bb61b4df7790ca43739e7a5463d4cf4cc6280b
Carlos Castillo <info@carloscastillo.cl>
2020-04-26 13:36:15 +0100

    Updates

---
+++ precache-manifest.81f951c74616c8b535a6920f44c70f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7564decd8b20ce9da65fb77af2dad15",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "de0817f72e5b3b8a6c85",
+    "url": "/recipes-app/static/css/main.45afaf59.chunk.css"
+  },
+  {
+    "revision": "3489384aae6ff5e655a3",
+    "url": "/recipes-app/static/js/2.e9ff4544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes-app/static/js/2.e9ff4544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0817f72e5b3b8a6c85",
+    "url": "/recipes-app/static/js/main.5856fd52.chunk.js"
+  },
+  {
+    "revision": "58699139534697a91c22",
+    "url": "/recipes-app/static/js/runtime-main.3ca22642.js"
+  }
+]);
